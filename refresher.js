f720c01d853b573aba82b99f708e743ba055d22e
Greg <grachkovsky@gmail.com>
2019-10-22 05:17:33 -0400

    captured the greeting in a variable and logged the variable

--- refresher.js
+++ refresher.js
@@ -1,7 +1,8 @@
 function createGreeting(){
 
-console.log('Hi my name is Slim Shady.');
+return('Hi my name is Slim Shady.');
 
 }
 
-createGreeting();
+const greeting1 = createGreeting();
+console.log(greeting1);
