dc9c2e056b511ae2bb96652e965b4f942d7f4ee6
Greg <grachkovsky@gmail.com>
2019-10-22 06:17:10 -0400

    redoing fresh again

--- refresher.js
+++ refresher.js
@@ -1,8 +1,2 @@
+console.log(`Hi my name is Greg and I am 31 years old`);
 
-function createGreeting(){
-    return (`Hi, my name is Greg and I'm 31 years old.`);
-}
-
-const greeting1 = createGreeting();
-
-console.log(greeting1);
